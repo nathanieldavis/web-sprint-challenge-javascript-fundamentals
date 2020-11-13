8bff2c6fc328c98f247b6aa598ed1956a1fac5d7
Nate Davis <orpheusdevelopment@gmail.com>
2020-11-13 12:03:45 -0600

    CodeGrade

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ myFunction();
 
 
 
-
 /* 🚀🚀🚀 Task 2: Counter 🚀🚀🚀 */
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
 function summation(/*Your Code Here*/) {
